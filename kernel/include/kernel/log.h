79ac50df3f838926dccbd45585db34e6c80bb356
Tronco2018 <tronco2018dev@gmail.com>
2026-08-17 18:04:15 +0200

    Changed log level

--- kernel/include/kernel/log.h
+++ kernel/include/kernel/log.h
@@ -1,17 +1,16 @@
 #ifndef LOG_H
 #define LOG_H
 
-enum LOG_LEVEL{ 
-	CRITICAL,
-	ERROR,
-	WARN,
-	INFO,
-	DEBUG,
-	TRACE
-};
+#define LOG_CRITICAL 0
+#define LOG_ERROR 1
+#define LOG_WARN 2
+#define LOG_INFO 3
+#define LOG_DEBUG 4
+#define LOG_TRACE 5
+
 
 void kernel_panic(const char* msg);
 void initialize_tty(void);
-void log_msg(enum LOG_LEVEL level, const char* msg);
+void log_msg(int log_lvl, const char* msg);
 
 #endif
