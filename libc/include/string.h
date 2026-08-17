4d09928abd2cf933891c9495933daa213b942df2
Tronco2018 <tronco2018dev@gmail.com>
2026-08-17 20:27:07 +0200

    Added 2 functions

--- libc/include/string.h
+++ libc/include/string.h
@@ -4,5 +4,6 @@
 #include <stddef.h>
 
 size_t strlen(const char*);
-
+char* strcpy(char* dst, const char* src);
+int strcmp(const char* s1, const char* s2);
 #endif
--- libc/string/strlen.c
+++ libc/string/strlen.c
@@ -8,3 +8,24 @@ size_t strlen(const char* str) {
 	return len;
 }
 
+char* strcpy(char* dst, const char* src) {
+	int dst_len = strlen(dst);
+	int src_len = strlen(src);
+
+	for (int i = 0; i < src_len; i++) {
+		dst[dst_len+i] = src[i];	
+	}
+	return dst;
+}
+
+int strcmp(const char* s1, const char* s2) {
+	int s1_len = strlen(s1);
+	int s2_len = strlen(s2);
+	if (s1_len != s2_len) return 1;
+	
+	for (int i = 0; i < s1_len; i++) {
+		if (s1[i] != s2[i]) return 1;
+	}
+	return 0;
+}
+
